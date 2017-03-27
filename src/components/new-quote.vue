350227032fe27d8c3f52c61de1d3bc72f03cc3ee
BERRAMOU2017 <azz-eddine.BERRAMOU@edu.uca.ac.ma>
2017-03-27 15:11:46 +0100

    adding files

---
+++ src/components/new-quote.vue
@@ -0,0 +1,40 @@
+<template>
+    <form @submit.prevent="onSubmited">
+
+        <div class="form-group">
+            <label for="content">Content</label>
+            <input type="text" name="content" id="content" class="form-control" v-model="quoteContent">
+        </div>
+
+        <button class="btn btn-primary">Submit</button>
+    </form>
+</template>
+<script>
+    import axios from 'axios';
+
+    export default{
+        data(){
+            return {
+
+                quoteContent: ''
+            }
+        },
+        methods: {
+
+            onSubmited(){
+
+                axios.post('http://127.0.0.1:8000/api/quote/',
+                        {content: this.quoteContent})
+                        .then(function (response) {
+
+                            console.log(response);
+
+                        }).catch(function (error) {
+
+                    console.log(error);
+
+                });
+            }
+        }
+    }
+</script>
---
+++ src/components/quotes.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <hr>
+        <button class="btn btn-primary" @click="onGetQuotes"> Get Quotes</button>
+        <hr>
+        <app-quote v-for="quote in quotes" :qt="quote"
+                   @quoteDeleted="onQuoteDeleted($event)"></app-quote>
+    </div>
+</template>
+
+<script>
+
+    import Quote from './quote.vue';
+    import axios from 'axios';
+
+    export default {
+        data(){
+            return {
+                quotes: []
+            }
+        },
+        components: {
+            'app-quote': Quote
+        },
+        methods: {
+            onGetQuotes(){
+
+                axios.get('http://127.0.0.1:8000/api/quotes')
+                        .then( response => {
+
+                            this.quotes = response.data.quotes
+                        })
+                        .catch( function (error) {
+
+                            console.log(error);
+                        });
+            },
+            onQuoteDeleted(id){
+                const  position = this.quotes.findIndex((element)=>{
+                            return element.id == id;
+                        });
+                this.quotes.splice(position, 1);
+            }
+
+
+        }
+    }
+
+</script>
